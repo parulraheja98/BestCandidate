1e4cc6d88019db0db09fbc21a875a4f1704eba7d
parulraheja98 <parulraheja98@gmail.com>
2019-07-07 00:44:58 -0500

    user auth functionality added

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restful import Api
+from resources.user import UserRegister
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.secret_key = 'praheja'
+api = Api(app)
+
+
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
