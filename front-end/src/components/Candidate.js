f980e63b5977722d7bdb023518ad6514922e67b5
Mario M <mariomendez1234@live.com.mx>
2019-07-17 01:06:29 -0500

    added images for landing page and candidate placeholder component

---
+++ front-end/src/components/Candidate.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Candidate extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is the candidat menu</h1>
+            </div>
+        )
+    }
+}
+
+export default Candidate
