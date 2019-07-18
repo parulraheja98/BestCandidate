aec68acd8a3eda500a8c5e798282fcca9acbab76
parulraheja98 <parulraheja98@gmail.com>
2019-07-18 02:01:46 -0500

    Removed unneccessary library

--- resources/job.py
+++ resources/job.py
@@ -5,7 +5,6 @@ from flask_restful import Resource, reqparse
 from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_claims, jwt_refresh_token_required, get_raw_jwt
 from datetime import datetime
 import json
-from bson import json_util
 
 class Job(Resource):
     def get(self, id):
