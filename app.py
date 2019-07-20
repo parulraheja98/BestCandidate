477061a4db374ce407aea15fdef7f67ac2ea6087
parulraheja98 <parulraheja98@gmail.com>
2019-07-20 09:00:07 -0500

    Modified the requirements, Docker and the app file

--- app.py
+++ app.py
@@ -100,5 +100,4 @@ api.add_resource(CandidatesByJob,'/jobs/<int:id>/candidates')
 from db import db
 db.init_app(app)
 if __name__ == '__main__':
-
-    app.run(port=5000, debug=True)
+    app.run(debug=True, host='0.0.0.0')
