acace40a8837b56e7884fd4dbd51c3e1d09c7086
Mario M <mariomendez1234@live.com.mx>
2019-07-22 22:44:14 -0500

    added  first name last name company and title to registration

--- front-end/src/components/layout/Register.js
+++ front-end/src/components/layout/Register.js
@@ -7,77 +7,132 @@ export class Register extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            username:'',
-            password:'',
-            confPassword:'',
-            email:'',
-            credentialsMatch:true,
-            role: '',
-            successsfulRegistration: false,
-            minLength: 3
+            username :'',
+            password :'',
+            firstname : '',
+            lastname : '',
+            title : '', // only for recruiter
+            company : '',//
+            confPassword :'',
+            credentialsMatch :true,
+            role : '',
+            successsfulRegistration : false,
+            minLength : 3,
+            showRecruiterFields : false,
+            allFieldsComplete : true
         };
     }
 
-    validateForm(){
-        return (this.state.username.length > this.state.minInputLength &&
-            this.state.password.length > this.state.minInputLength);
-    }
     
     handleChange = event => {
-        //console.log(event.target.id+" updating to "+event.target.value);
-        console.log("the ussername is "+this.state.username);
-        console.log("the password is "+this.state.password);
+        console.log(event.target.id+" updating to "+event.target.value);
         this.setState({
           [event.target.id]: event.target.value
         });
-      }
+    }
     
-      handleRole(event) {
-          console.log('event name ', event.target.name);
-           console.log('checking event', event.target.checked);
-           console.log('checking value', event.target.value);
-           this.setState({role: event.target.value});
-      }
+    handleRole(event) {
+        console.log('role changed to ', event.target.value);
+        this.setState({role: event.target.value});
+    }
+
+    verifyState = () => {
+        if(this.state.role == "candidate"){
+            if(typeof this.state.username !== 'undefined' && typeof this.state.firstname !== 'undefined'
+                    && typeof this.state.lastname !== 'undefined'){
+                        console.log("all fields for candidate filled");
+                        return true;
+            }
+        }else if(this.state.role === "recruiter"){
+            if(typeof this.state.username !== 'undefined' && typeof this.state.firstname !== 'undefined'
+                    && typeof this.state.lastname !== 'undefined' && typeof this.state.title !== 'undefined'
+                    && typeof this.state.company !== 'undefined'){
+                        console.log("all fields for recruiter filled");
+                        return true;
+            }
+        }
+        return false;
+    }
+
+    
 
     handleSubmit(event) {
 
         event.preventDefault();
-        console.log("sending "+this.state.username+" and "+this.state.password);
+        console.log("sending "+this.state.username+" and "+this.state.password+" as "+this.state.role);
 
-        if(this.state.password === this.state.confPassword){
-   
-            console.log("Passwords match");
+        if(this.verifyState()){
+            if(this.state.password === this.state.confPassword){
+    
+                console.log("Passwords match");
+                
+                var candidateRoute = 'http://localhost:5000/candidates'
+                var recruiterRoute = 'http://localhost:5000/recruiters'
+                var route;
 
-            var registerCredentials = JSON.stringify({
-                username:this.state.username,
-                password:this.state.password,
-                role: this.state.role
-            })
+                var registrationData;
 
-            fetch('http://localhost:5000/register' , {
-            method:'POST',
-            credentials:'include',
-            headers:{'Content-Type':'application/json'},
-            body:registerCredentials
-            })
-            .then(response => response.json())
-            .then(response => {
-            console.log('received response');
-            console.log(response);
-            if(response.message) {
-                console.log("Successful registration"); //display succesful registration alert
-                this.setState({successsfulRegistration: true});
-                this.props.history.push('/candidate'); //redirect to candidat page
-            }
-            
-            })
+                if( this.state.role === "candidate"){
+                    console.log("packing post as candidate");
+                    registrationData = JSON.stringify({
+                        username : this.state.username,
+                        password : this.state.password,
+                        firstname : this.state.firstname,
+                        lastname : this.state.lastname,
+                        role: this.state.role
+                    })
+                    route = candidateRoute;
+                }else if( this.state.role === "recruiter"){
+                    console.log("packing post as recruiter");
+                    registrationData = JSON.stringify({
+                        username : this.state.username,
+                        password : this.state.password,
+                        firstname : this.state.firstname,
+                        lastname : this.state.lastname,
+                        title : this.state.title,
+                        company : this.state.company
+                    })
+                    route = recruiterRoute;
+                }
+
+                //only proceed if the route has been assigned
+                if(typeof route !== 'undefined' && typeof registrationData !== 'undefined'){
+                    console.log("Route has been assigned ", route, registrationData);
+                    fetch(route , {
+                    method:'POST',
+                    credentials:'include',
+                    headers:{'Content-Type':'application/json'},
+                    body: registrationData
+                    })
+                    .then(response => response.json())
+                    .then(response => {
+                    console.log('Received response');
+                    console.log(response);
+                    if(response.ok) {
+                        console.log("Successful registration"); //display succesful registration alert
+                        this.setState({successsfulRegistration: true});
+                    
+                        //redirect to the appropiate login page
+                        if(this.state.role === "recruiter"){
+                            this.props.history.push('/recruiter-login'); //redirect to candidat page
+                        }else if(this.state.role === "candidate"){
+                            this.props.history.push('/candidate-login'); //redirect to candidat page
+                        }
+                    }
+                    
+                    })
+                }else{
+                    console.log("role or registration payload are undefined");
+                }    
+            }else{
+                console.log(this.state.password+" match "+this.state.confPassword);
+                this.setState({credentialsMatch:false, successsfulRegistration:false}); //display credentials match alert
+                console.log("Passwords do not match");
+            }    
         }else{
-            console.log(this.state.password+" match "+this.state.confPassword);
-            this.setState({credentialsMatch:false, successsfulRegistration:false}); //display credentials match alert
-            console.log("Passwords do not match");
-
-        }    
-        
+            console.log("All fields are not complete");
+            this.setState({allFieldsComplete: false});
+        }
     }
     
 
@@ -90,13 +145,41 @@ export class Register extends Component {
                 <h2>Create an account</h2> 
                 <br/>
                <Form style={this.getFormStyle()}> 
-                    {this.state.credentialsMatch ?null :<Alert variant='danger'>{this.state.role} credentials do not match.</Alert>}
-                    {this.state.successsfulRegistration ?<Alert variant='success'>{this.state.role} account created.</Alert>:null}
+                    {this.state.credentialsMatch ? null :<Alert variant='danger'>{this.state.role} credentials do not match.</Alert>}
+                    {this.state.allFieldsComplete ? null :<Alert variant='warning'>Please enter all fields..</Alert>}
+                    {this.state.successsfulRegistration ?<Alert variant='success'>{this.state.role} account created.</Alert>:null}                   
+                    <Form.Group controlId="firstname">    
+                        <Form.Control autoFocus type='text'  value={this.state.firstname} placeholder='first name' onChange={this.handleChange} required/>
+                    </Form.Group>
+                    <Form.Group controlId="lastname">    
+                        <Form.Control  type='text'  value={this.state.lastname} placeholder='last name' onChange={this.handleChange} required/>
+                    </Form.Group>
                     <Form.Group controlId="username">    
-                        <Form.Control autoFocus type='text'  value={this.state.username} placeholder='Enter username' onChange={this.handleChange} required/>
+                        <Form.Control autoFocus type='text'  value={this.state.username} placeholder='username' onChange={this.handleChange} required/>
                         <Form.Text className="text"> username must be at least {this.state.minLength} characters long.</Form.Text>
                     </Form.Group>
 
+                    <br/>
+                    <h5>I am a: </h5>
+                    <ButtonGroup toggle className="mt-2" style={this.getCheckboxStyle()}>
+                        <br/>
+                        <ToggleButton defaultChecked type="radio" name="radio"  value="candidate" onChange={this.handleRole.bind(this)}>Candidate</ToggleButton>
+                        <ToggleButton  type="radio" name="radio" value="recruiter" onChange={this.handleRole.bind(this)}>Recruiter</ToggleButton>
+                    </ButtonGroup>
+                    <br/>
+
+                    {this.state.role === "recruiter" ? 
+                    <div>
+                        <Form.Group controlId="title">    
+                            <Form.Control autoFocus type='text'  value={this.state.title} placeholder='Job title' onChange={this.handleChange} required/>
+                        </Form.Group> 
+                        <Form.Group controlId="company">    
+                            <Form.Control type='text'  value={this.state.company} placeholder='Company name' onChange={this.handleChange} required/>
+                        </Form.Group>
+                    </div>    
+                    : null                    
+                    }
+                     <br/>
                     <Form.Group controlId="password">
                         <Form.Control type="password" value={this.state.password} onChange={this.handleChange} placeholder="Password" required/>
                     </Form.Group>
@@ -107,22 +190,21 @@ export class Register extends Component {
                     </Form.Group>
                     
                     
-                    <br/>
-                    <h5>I am a: </h5>
-                    <ButtonGroup toggle className="mt-2">
-                        <ToggleButton type="radio" name="radio" defaultChecked value="candidate" onChange={this.handleRole.bind(this)}>Candidate</ToggleButton>
-                        <ToggleButton  type="radio" name="radio" value="recruiter" onChange={this.handleRole.bind(this)}>Recruiter</ToggleButton>
-                    </ButtonGroup>
-                    
-                    <br/>
-                    <br/>
-                    <Button block  variant="primary" onClick={this.handleSubmit.bind(this)} type="submit">Register</Button>
+                    <Button block  variant="primary" onClick={this.handleSubmit.bind(this)} type="submit">Register {this.state.role}</Button>
                 </Form>
             </div>
             
         )
     }
 
+    
+
+    getCheckboxStyle = () => {
+        return {
+            marginTop : '20px',
+            marginBottom : '20px'
+        }
+    }
 
     getLoginDivStyle = () => {
         return {
